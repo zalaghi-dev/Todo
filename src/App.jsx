9747f72f651f5764d514ec14508c76f98bfecd62
HenryAllen <HenryAllen.dev@gmail.com>
2022-02-17 23:16:23 +0330

    fixin app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./layout/Header";
 import "./assets/fonts/fontawesome/css/all.css";
