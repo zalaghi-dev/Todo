7d5ec5b98e74ca21d81c2c5b7aef830048a62837
HenryAllen <HenryAllen.dev@gmail.com>
2022-02-17 23:53:41 +0330

    Commenting localStorage bcs of gh-pages

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,13 @@ import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { reducers } from "../reducers";
 export const store = createStore(reducers, compose(applyMiddleware(thunk)));
-store.subscribe(() => {
-  localStorage.setItem(
-    "save",
-    JSON.stringify(store.getState().activityReducer)
-  );
-});
-store.dispatch({
-  type: "INIT",
-  payload: JSON.parse(localStorage.getItem("save")),
-});
+// store.subscribe(() => {
+//   localStorage.setItem(
+//     "save",
+//     JSON.stringify(store.getState().activityReducer)
+//   );
+// });
+// store.dispatch({
+//   type: "INIT",
+//   payload: JSON.parse(localStorage.getItem("save")),
+// });
